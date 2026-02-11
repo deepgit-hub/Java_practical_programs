85ec0d105c02c2a57551c69b80006e3daf204abf
Deepak <darkdeepak001@gmail.com>
2026-02-11 20:50:41 +0530

    Create 3c.java

---
+++ 3c.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.System;
+class 3c
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scan.nextInt();
+        for(int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
