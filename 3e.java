9977bd78ce68590d690bb37de27a20dc59eb2ae0
Deepak <darkdeepak001@gmail.com>
2026-02-11 20:54:40 +0530

    Update 3e.java

--- 3e.java
+++ 3e.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-
-class 3
+import java.lang.System;
+class 3e
 {
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int rows = scan.nextInt();
-        int num = 1;   // starting number
+        int num = 1;  
         for(int i = 1; i <= rows; i++)
         {
             for(int j = 1; j <= i; j++)
             {
                 System.out.print(num + " ");
-                num++;   // increase number each time
+                num++; 
             }
             System.out.println();
         }
